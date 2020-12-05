0b203b9692a7ce480896cbf47560cfa618fe1281
umunadine <umutesinad3@gmail.com>
2020-12-05 18:25:52 +0200

    creating forms

--- pitch.py
+++ pitch.py
@@ -1,15 +1,30 @@
 from flask import Flask, render_template, url_for
 from app import app 
 
+posts = [
+    {
+        'author': 'Nadine',
+        'title': 'Pitch 1',
+        'content': 'This is the first post',
+        'date_posted': ' Dec 05, 2020'
+    },
+    {
+        'author': 'Dannu',
+        'title': 'Pitch 2',
+        'content': 'This is the second post',
+        'date_posted': 'Aug 01, 2020'
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html',posts=posts)
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html',title='About')
      
 
 
