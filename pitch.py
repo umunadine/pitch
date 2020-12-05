744ab0794e6d56300ef79e949b35393940d5e93e
umunadine <umutesinad3@gmail.com>
2020-12-06 00:29:33 +0200

    ccreating db

--- pitch.py
+++ pitch.py
@@ -5,7 +5,7 @@ from app import app
 from forms import RegistrationForm,LoginForm
 
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba205'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pitch.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 class User(db.Model):
