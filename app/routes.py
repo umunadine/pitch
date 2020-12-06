b5abe2d675bcd48b142649ed0ca1fb966ee027c3
umunadine <umutesinad3@gmail.com>
2020-12-06 19:55:07 +0200

    updating user profile

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, flash,url_for, redirect
+from flask import render_template, flash,url_for, redirect, request
 from app import app,db, bcrypt
-from app.forms import RegistrationForm,LoginForm
+from app.forms import RegistrationForm,LoginForm,UpdateAccountForm
 from app.models import User,Post
-from flask_login import login_user,current_user,logout_user
+from flask_login import login_user,current_user,logout_user,login_required
 
 posts = [
     {
@@ -51,7 +51,8 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password,form.password.data):
             login_user(user, remember= form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else :
              flash('Login Unsuccessful. Please check username and password', 'danger')   
     return render_template('login.html', title='Login', form=form)
@@ -60,4 +61,21 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+
+@app.route("/account",methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Succcessfully updated','success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file= url_for('static',filename='profile_pics/'+ current_user.image_file)
+    return render_template('account.html', title='account', image_file = image_file, form=form)
   
